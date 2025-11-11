4d53e316217cdfd8012f5fdd7c7d135f3218330c
youssefhisham157-cloud <youssefhisham157@gmail.com>
2025-11-11 09:58:13 +0200

    Update Student.h

--- Student.h
+++ Student.h
@@ -4,23 +4,18 @@
 #include "Person.h"
 #include <string>
 #using namespace std;
-class Student{
+
+class Student : public Person {
 private:
-int yearLevel;
-string major;
+    int yearLevel;
+    string major;
 public:
-Student(int y, string m, 
-display()
-
-
-
-
-
-
-
-
-
-
-
+    Student(string n, int i, int y, string m);
+    Student();
+    void display();
+    string getName();
+    int getID();
+    ~Student();
+};
 
 #endif
