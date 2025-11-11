c34d6764d8a15920f7f48a330bbecd405897cb66
youssefhisham157-cloud <youssefhisham157@gmail.com>
2025-11-11 10:04:04 +0200

    Update Person.h

--- Person.h
+++ Person.h
@@ -5,13 +5,15 @@
 using namespace std;
 
 class Person {
-protected:
+private:
     string name;
     int id;
 public:
     Person(string n, int i);
     Person();
     void display();
+    string getName();
+    int getID();
     ~Person();
 };
 
