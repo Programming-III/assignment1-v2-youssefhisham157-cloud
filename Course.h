9b5a34e94acc68830bbbd11b36057183f8797b0a
youssefhisham157-cloud <youssefhisham157@gmail.com>
2025-11-11 09:56:25 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -1,22 +1,23 @@
 #ifndef COURSE_H
 #define COURSE_H
 
-#include <string>
 #include "Student.h"
+
 using namespace std;
-//#write your code here
 
-
-
-
-
-
-
-
-
-
-
-
-
+class Course {
+private:
+    string courseCode;
+    string courseName;
+    int maxStudents;
+    Student* students;
+    int currentStudents;
+public:
+    Course(string code, string name, int max);
+    Course();
+    void addStudent(const Student& s);
+    void displayCourseInfo();
+    ~Course();
+};
 
 #endif
